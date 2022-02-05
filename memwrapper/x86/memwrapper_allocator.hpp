7067cb6ae304b73f1962617e52139808928866d6
Musaigen <fedya.rezan@gmail.com>
2022-02-06 01:02:50 +0200

    Add files via upload

---
+++ memwrapper/x86/memwrapper_allocator.hpp
@@ -0,0 +1,112 @@
+﻿#ifndef MEMWRAPPER_ALLOCATOR_HPP_
+#define MEMWRAPPER_ALLOCATOR_HPP_
+
+namespace memwrapper
+{
+	class basic_allocator
+	{
+	protected:
+		uint8_t* m_code;
+		uint32_t m_size;
+		uint32_t m_offset;
+	public:
+		basic_allocator(uint32_t size = 4096) :
+			m_code(nullptr),
+			m_size(0),
+			m_offset(0)
+		{
+			SYSTEM_INFO sysinfo{ 0 };
+			GetSystemInfo(&sysinfo);
+
+			m_size = detail::align_value(size, sysinfo.dwPageSize);
+			m_code = reinterpret_cast<uint8_t*>(
+				VirtualAlloc(NULL, m_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)
+			);
+
+			std::printf("%X\n", reinterpret_cast<uint32_t>(m_code));
+		}
+
+		// writing manipulations
+		void db(uint8_t opcode)
+		{
+			if ((m_offset + sizeof(uint8_t)) > m_size)
+				return;
+
+			m_code[m_offset++] = opcode;
+		}
+
+		template<typename T>
+		void db(T* object, uint32_t size)
+		{
+			uint8_t* oplist{ reinterpret_cast<uint8_t*>(object) };
+
+			for (uint32_t i = 0; i < size; i++)
+				db(oplist[i]);
+		}
+
+		template<typename T>
+		void dbvalue(T value)
+		{
+			if constexpr (sizeof(T) > 1)
+			{
+				detail::byteof<T> byteof{ value };
+
+				for (size_t i = 0; i < sizeof(T); i++)
+					db(byteof.bytes[i]);
+			}
+			else
+				db(static_cast<uint8_t>(value));
+		}
+
+		template<typename T, typename... Args>
+		void dbvalues(T value, Args... args)
+		{
+			dbvalue(value);
+
+			if constexpr (sizeof...(Args) > 1)
+				dbvalues(args...);
+			else
+				dbvalue(args...);
+		}
+
+		// code pointer manipulations
+		uint8_t* begin()				{ return m_code; }
+		uint8_t* now()					{ return &m_code[m_offset]; }
+		uint8_t* end()					{ return &m_code[m_size]; }
+		uint8_t* get(uint32_t offset)	{ return &m_code[offset]; }
+
+		void free() { VirtualFree(m_code, NULL, MEM_RELEASE); }
+		void ready() { flush_memory(m_code, m_size); }
+
+		// offset manipulations
+		uint32_t get_offset()
+		{ return m_offset; }
+
+		void set_offset(uint32_t offset)
+		{
+			if ((offset < 0) || (offset >= m_size))
+				return;
+
+			m_offset = offset;
+		}
+	}; // !class basic_allocator
+
+	class asm_allocator : public basic_allocator
+	{
+	public:
+		asm_allocator(uint32_t size = 4096) :
+			basic_allocator(size)
+		{}
+
+		// yeah, another class for one function
+		void jmp(const memory_pointer& to)
+		{
+			auto rel32 = detail::get_relative_address(to, now());
+
+			db(0xE9);
+			dbvalue(rel32);
+		}
+	}; // !class asm_allocator : public basic_allocator
+}
+
+#endif // !MEMWRAPPER_ALLOCATOR_HPP_
---
+++ memwrapper/x86/memwrapper_hook.hpp
@@ -0,0 +1,301 @@
+﻿#ifndef MEMWRAPPER_HOOK_HPP_
+#define MEMWRAPPER_HOOK_HPP_
+
+namespace memwrapper
+{
+	template<typename Function>
+	class memhook
+	{
+	protected:
+		using memhook_original_code_t	= std::unique_ptr<uint8_t[]>;
+		using memhook_code_gen_t		= std::unique_ptr<asm_allocator>;
+		
+		using function_info = detail::function_analyzer<Function>;
+		using return_type = typename function_info::return_type;
+
+		memory_pointer m_address;
+		memory_pointer m_callback;
+
+		size_t m_size;
+
+		memhook_original_code_t m_original_code;
+		memhook_code_gen_t		m_code;
+
+		bool m_installed;
+		bool m_listing_broken;
+		bool m_is_call_instruction;
+
+		uint32_t m_call_abs_address;
+	public:
+		memhook(const memory_pointer& address, const memory_pointer& callback) :
+			m_address(address),
+			m_callback(callback),
+			m_size(0),
+			m_installed(false),
+			m_listing_broken(false)
+		{
+			auto cursor = m_address.get<uint8_t*>();
+
+			while (m_size < 5)
+			{
+				hde32s hs;
+				hde32_disasm(cursor, &hs);
+
+				m_listing_broken = (hs.flags & F_ERROR);
+				if (m_listing_broken)
+					break;
+
+				cursor += hs.len;
+				m_size += hs.len;
+			}
+		}
+
+		~memhook()
+		{
+			remove();
+		}
+
+		void install()
+		{
+			if (m_listing_broken)
+				return;
+
+			if (m_installed)
+				return;
+
+			if (!is_executable(m_address))
+				return;
+
+			if (m_code)
+			{
+				// installing jmp to callback again
+				m_code->set_offset(0);
+				m_code->jmp(m_callback);
+				
+				// marking as installed
+				m_installed = true;
+				return;
+			}
+			else
+			{
+				hde32s hs;
+				hde32_disasm(m_address, &hs);
+
+				if (hs.opcode == 0xE8)
+				{
+					m_is_call_instruction = true;
+					m_call_abs_address = detail::restore_absolute_address(hs.imm.imm32, m_address);
+				}
+			}
+
+			// creating original code and trampoline code instances
+			m_original_code = std::make_unique<uint8_t[]>(m_size);
+			m_code			= std::make_unique<asm_allocator>();
+
+			// copying original instructions from the target address
+			copy_memory(m_original_code.get(), m_address, m_size);
+
+			// generating trampoline
+			auto address = m_address.get_int();
+			auto detour = address + m_size;
+
+			m_code->jmp(m_callback);
+
+			if(!m_is_call_instruction)
+				generate_trampoline();
+
+			m_code->ready();
+
+			// installing patch
+			auto rel32 = detail::get_relative_address(m_code->begin(), address);
+
+			if(!m_is_call_instruction)
+				write_memory<uint8_t>(address, 0xE9);
+
+			write_memory<uint32_t>(address + 1, rel32);
+
+			if (m_size > 5)
+				fill_memory(address + 5, 0x90, m_size - 5);
+
+			// mark as installed
+			m_installed = true;
+		}
+
+		void remove()
+		{
+			if (!m_installed)
+				return;
+
+			hde32s hs;
+			hde32_disasm(m_address, &hs);
+			if ((hs.flags & F_ERROR) ||
+				!(hs.flags & F_RELATIVE) || 
+				!(hs.flags & F_IMM32))
+			{
+				// trying to restore listing/original code
+				copy_memory(m_address, m_original_code.get(), m_size);
+
+				// unloading pointers
+				m_code->free();
+
+				m_original_code.reset();
+				m_code.reset();
+
+				// marking as uninstalled
+				m_installed = false;
+				m_is_call_instruction = false;
+				return;
+			}
+
+			auto destination = detail::restore_absolute_address(hs.imm.imm32, m_address, hs.len);
+			auto code_ptr = reinterpret_cast<uint32_t>(m_code->begin());
+
+			if (destination == code_ptr)
+			{
+				// trying to restore listing
+				copy_memory(m_address, m_original_code.get(), m_size);
+
+				// unloading pointers
+				m_code->free();
+
+				m_original_code.reset();
+				m_code.reset();
+
+				// updating states
+				m_is_call_instruction = false;
+			}
+			else
+			{
+				if (m_is_call_instruction)
+				{
+					// Redirecting jmp to hooked function
+					m_code->set_offset(0);
+					m_code->jmp(m_call_abs_address);
+				}
+				else
+				{
+					// Removing jmp to avoid crash/calling detour
+					fill_memory(code_ptr, 0x90, 0x5);
+				}
+			}
+
+			// marking as uninstalled
+			m_installed = false;
+		}
+
+		template<typename... Args>
+		return_type call(Args... args)
+		{
+			return call_function<return_type, function_info::call_convention, Args...>(get_trampoline(), args...);
+		}
+
+		uintptr_t get_trampoline()
+		{
+			auto code = reinterpret_cast<uint32_t>(m_code->get(5));
+			return (m_is_call_instruction) ? (m_call_abs_address) : (code);
+		}
+	private:
+		void generate_trampoline()
+		{
+#pragma pack(push, 1)
+			struct
+			{
+				uint8_t opcode;
+				uint32_t operand;
+			} jmp, call;
+
+			struct
+			{
+				uint8_t opcode;
+				uint8_t opcode2;
+				uint32_t operand;
+			} jcc;
+#pragma pack(pop)
+
+			uint8_t* now	= m_address;
+
+			uint32_t step	= 0;
+
+			bool finished = false;
+			while (!finished)
+			{
+				if (step >= m_size)
+				{
+					m_code->jmp(now);
+					break;
+				}
+
+				void* opcode;
+				uint32_t oplen;
+
+				hde32s hs;
+				uint32_t len = hde32_disasm(now, &hs);
+
+				if (hs.flags & F_ERROR)
+					break;
+
+				if (hs.opcode == 0xE8)
+				{
+					auto destination = detail::restore_absolute_address(hs.imm.imm32, now);
+					auto rel32 = detail::get_relative_address(destination, m_code->now());
+
+					call.opcode = 0xE8;
+					call.operand = rel32;
+
+					opcode = &call;
+					oplen = sizeof(call);
+				}
+				else if ((hs.opcode & 0xFD) == 0xE9)
+				{
+					auto destination = reinterpret_cast<uint32_t>(now + hs.len);
+					if (hs.opcode == 0xEB)
+						destination += hs.imm.imm8;
+					else
+						destination += hs.imm.imm32;
+					
+					auto rel32 = detail::get_relative_address(destination, m_code->now());
+
+					jmp.opcode = 0xE9;
+					jmp.operand = rel32;
+
+					opcode = &jmp;
+					oplen = sizeof(jmp);
+				}
+				else if ((hs.opcode & 0xF0) == 0x70 ||
+					(hs.opcode2 & 0xF0) == 0x80)
+				{
+					auto destination = reinterpret_cast<uint32_t>(now + hs.len);
+					if ((hs.opcode & 0xF0) == 0x70)
+						destination += hs.imm.imm8;
+					else
+						destination += hs.imm.imm32;
+
+					auto cond = ((hs.opcode != 0x0F ? hs.opcode : hs.opcode2) & 0x0F);
+					auto rel32 = detail::get_relative_address(destination, m_code->now(), sizeof(jcc));
+
+					jcc.opcode = 0x0F;
+					jcc.opcode2 = (0x80 | cond);
+					jcc.operand = rel32;
+
+					opcode = &jcc;
+					oplen = sizeof(jcc);
+				}
+				else
+				{
+					opcode = now;
+					oplen = len;
+				}
+
+				// adding instruction to list
+				m_code->db(opcode, oplen);
+
+				// shifting cursor
+				step += oplen;
+				now += len;
+			}
+		}
+	}; // !class memhook<Function>
+
+} // !namespace memwrapper
+
+#endif // !MEMWRAPPER_HOOK_HPP_
